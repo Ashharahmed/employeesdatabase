ab1e00c851b108f78b1e8fc508182d7520c65948
Ashharahmed <69009874+Ashharahmed@users.noreply.github.com>
2021-02-02 22:58:57 +1100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "empdatabase-12.cwzcccaqrmsz.us-east-1.rds.amazonaws.com"
+customuser = "Ashman"
+custompass = "Ashman97"
+customdb = "employees"
+custombucket = "employeesadd"
+customregion = "us-east-1"
